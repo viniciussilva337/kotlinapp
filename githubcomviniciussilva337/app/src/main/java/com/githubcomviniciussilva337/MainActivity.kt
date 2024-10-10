20936c8811263b325ed0b642256dc31ab26f863e
vinicius silva <85956064+viniciussilva337@users.noreply.github.com>
2024-10-09 21:18:24 -0300

    Add files via upload

---
+++ githubcomviniciussilva337/app/src/main/java/com/githubcomviniciussilva337/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.githubcomviniciussilva337
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+
+
+
+    }
+}
+
